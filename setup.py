6d64c277574aa2dbf5a3428e3c2b7e4adac59296
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-06-22 18:58:46 +0200

    added initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "peters_example_code",
+    author = "peter",
+    version = 0,
+    dependency_links = (),
+    install_requires = ['numpy', 'matplotlib'],
+    scripts = [],
+    packages=find_packages(),
+    )
